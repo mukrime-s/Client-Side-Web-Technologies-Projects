ad509ff4b7bc6d3dcb64e34c73838014b9771e80
mükrime sağıroğlu <102221568+mukrime-s@users.noreply.github.com>
2022-11-26 21:37:17 +0300

    Erors fixed

--- yks-score/yks-score.js
+++ yks-score/yks-score.js
@@ -21,74 +21,96 @@ function calcNet(num1, num2) {
     log.innerHTML += `<td>${net}</td>`;
 }
 */
+var TurkceDogru, TurkceYanlis, Turkcenet, turkceToplam;
+var sosyalDogru, sosyalYanlis, sosyalnet, sosyalToplam;
+var tmatematikDogru, tmatematikYanlis, tmatematiknet, tmatematikToplam;
+var fenDogru, fenYanlis, fennet, fenToplam;
+
+var edebiyatDogru, edebiyatYanlis, aytEdebiyatNet, edebiyatToplam;
+var tarih1Dogru, tarih1Yanlis, aytTarih1Net, tarih1Toplam;
+var cografya1Dogru, cografya1Yanlis, aytCografya1Net, cografya1Toplam;
+var tarih2Dogru, tarih2Yanlis, aytTarih2Net, tarih2Toplam;
+var cografya2Dogru, cografya2Yanlis, aytCografya2Net, cografya2Toplam;
+var felsefeDogru, felsefeYanlis, aytFelsefeNet, felsefeToplam;
+var dinDogru, dinYanlis, aytDinNet, dinToplam;
+
+var matematikDogru, matematikYanlis, aytMatematikNet, matematik2Toplam;
+var fizikDogru, fizikYanlis, aytFizikNet, fizikToplam;
+var kimyaDogru, kimyaYanlis, aytKimyaNet, kimyaToplam;
+var BiyolojiDogru, BiyolojiYanlis, aytBiyolojiNet, BiyolojiToplam;
+
+var Diplomanotu, OBP;
+
+
 
 elem.onclick = function() {
  /**
 * @brief  Calculations and warnings in tyt subtitle
 */
-    var TurkceDogru = Number(document.getElementById("TurkceDogru").value); //turkce net calculated
-    var TurkceYanlis = Number(document.getElementById("TurkceYanlis").value);
-    var Turkcenet = TurkceDogru   -  TurkceYanlis   *   0.25
+    TurkceDogru = Number(document.getElementById("TurkceDogru").value); //turkce net calculated
+    TurkceYanlis = Number(document.getElementById("TurkceYanlis").value);
+    Turkcenet = TurkceDogru   -  TurkceYanlis   *   0.25
     document.getElementById("tytTurkceNet").innerHTML =Turkcenet;
-    var turkceToplam=TurkceDogru+TurkceYanlis
+    turkceToplam=TurkceDogru+TurkceYanlis
     if(turkceToplam>40){//turkce alert
         window.alert("Toplamda 40 soru girebilirsin.");
         document.getElementById("TurkceDogru").innerText =" ";
         document.getElementById("TurkceYanlis").innerText =" ";
     }
     
-    var sosyalDogru = Number(document.getElementById("sosyalDogru").value);//social net calculated
-    var sosyalYanlis = Number(document.getElementById("sosyalYanlis").value);
-    var sosyalnet = sosyalDogru   -  sosyalYanlis   *   0.25
+    sosyalDogru = Number(document.getElementById("sosyalDogru").value);//social net calculated
+    sosyalYanlis = Number(document.getElementById("sosyalYanlis").value);
+    sosyalnet = sosyalDogru   -  sosyalYanlis   *   0.25
     document.getElementById("tytSosyalNet").innerHTML =sosyalnet;
-    var sosyalToplam=sosyalDogru+sosyalYanlis//social alert
+    sosyalToplam=sosyalDogru+sosyalYanlis//social alert
     if(sosyalToplam>20){
         window.alert("Toplamda 20 soru girebilirsin.");
     }
 
-    var tmatematikDogru = Number(document.getElementById("tmatematikDogru").value);//math net calculated
-    var tmatematikYanlis = Number(document.getElementById("tmatematikYanlis").value);
-    var tmatematiknet = tmatematikDogru   -  tmatematikYanlis   *   0.25
+    tmatematikDogru = Number(document.getElementById("tmatematikDogru").value);//math net calculated
+    tmatematikYanlis = Number(document.getElementById("tmatematikYanlis").value);
+    tmatematiknet = tmatematikDogru   -  tmatematikYanlis   *   0.25
     document.getElementById("tytTmatematikNet").innerHTML =tmatematiknet;
-    var tmatematikToplam=tmatematikDogru+tmatematikYanlis
+    tmatematikToplam=tmatematikDogru+tmatematikYanlis
     if(tmatematikToplam>40){//math alert
         window.alert("Toplamda 40 soru girebilirsin.");
     }
 
-    var fenDogru = Number(document.getElementById("fenDogru").value);//science net calculated
-    var fenYanlis = Number(document.getElementById("fenYanlis").value);
-    var fennet = fenDogru   -  fenYanlis   *   0.25
+    fenDogru = Number(document.getElementById("fenDogru").value);//science net calculated
+    fenYanlis = Number(document.getElementById("fenYanlis").value);
+    fennet = fenDogru   -  fenYanlis   *   0.25
     document.getElementById("tytFenNet").innerHTML =fennet;
-    var fenToplam=fenDogru+fenYanlis
+    fenToplam=fenDogru+fenYanlis
     if(fenToplam>20){//science alert
         window.alert("Toplamda 20 soru girebilirsin.");
     }
 /**
 * @brief  Calculations and warnings in ayt subtitle
 */
-    var edebiyatDogru = Number(document.getElementById("edebiyatDogru").value);
-    var edebiyatYanlis = Number(document.getElementById("edebiyatYanlis").value);
-    var aytEdebiyatNet = edebiyatDogru   -  edebiyatYanlis   *   0.25
+
+    edebiyatDogru = Number(document.getElementById("edebiyatDogru").value);
+    edebiyatYanlis = Number(document.getElementById("edebiyatYanlis").value);
+    aytEdebiyatNet = edebiyatDogru   -  edebiyatYanlis   *   0.25
     document.getElementById("aytEdebiyatNet").innerHTML=aytEdebiyatNet;
-    var edebiyatToplam=edebiyatDogru+edebiyatYanlis
+    edebiyatToplam=edebiyatDogru+edebiyatYanlis
     if(edebiyatToplam>24){
         window.alert("Toplamda 24 soru girebilirsin.");
     }
 
-    var tarih1Dogru = Number(document.getElementById("tarih1Dogru").value);
-    var tarih1Yanlis = Number(document.getElementById("tarih1Yanlis").value);
-    var aytTarih1Net = tarih1Dogru   -  tarih1Yanlis   *   0.25
+    tarih1Dogru = Number(document.getElementById("tarih1Dogru").value);
+    tarih1Yanlis = Number(document.getElementById("tarih1Yanlis").value);
+    aytTarih1Net = tarih1Dogru   -  tarih1Yanlis   *   0.25
     document.getElementById("aytTarih1Net").innerHTML =aytTarih1Net;
-    var tarih1Toplam=tarih1Dogru+tarih1Yanlis
+    tarih1Toplam=tarih1Dogru+tarih1Yanlis
     if(tarih1Toplam>10){
         window.alert("Toplamda 10 soru girebilirsin.");
     }
 
-    var cografya1Dogru = Number(document.getElementById("cografya1Dogru").value);
-    var cografya1Yanlis = Number(document.getElementById("cografya1Yanlis").value);
-    var aytCografya1Net = cografya1Dogru   -  cografya1Yanlis   *   0.25
+    cografya1Dogru = Number(document.getElementById("cografya1Dogru").value);
+    cografya1Yanlis = Number(document.getElementById("cografya1Yanlis").value);
+    aytCografya1Net = cografya1Dogru   -  cografya1Yanlis   *   0.25
     document.getElementById("aytCografya1Net").innerHTML =aytCografya1Net;
-    var cografya1Toplam=cografya1Dogru+cografya1Yanlis
+    cografya1Toplam=cografya1Dogru+cografya1Yanlis
     if(cografya1Toplam>6){
         window.alert("Toplamda 6 soru girebilirsin.");
     }
@@ -102,65 +124,65 @@ elem.onclick = function() {
         window.alert("Toplamda 11 soru girebilirsin.");
     }
 
-    var cografya2Dogru = Number(document.getElementById("cografya2Dogru").value);
-    var cografya2Yanlis = Number(document.getElementById("cografya2Yanlis").value);
-    var aytCografya2Net = cografya2Dogru   -  cografya2Yanlis   *   0.25
+    cografya2Dogru = Number(document.getElementById("cografya2Dogru").value);
+    cografya2Yanlis = Number(document.getElementById("cografya2Yanlis").value);
+    aytCografya2Net = cografya2Dogru   -  cografya2Yanlis   *   0.25
     document.getElementById("aytCografya2Net").innerHTML =aytCografya2Net;
-    var cografya2Toplam=cografya2Dogru+cografya2Yanlis
+    cografya2Toplam=cografya2Dogru+cografya2Yanlis
     if(cografya2Toplam>11){
         window.alert("Toplamda 11 soru girebilirsin.");
     }
 
-    var felsefeDogru = Number(document.getElementById("felsefeDogru").value);
-    var felsefeYanlis = Number(document.getElementById("felsefeYanlis").value);
-    var aytFelsefeNet = felsefeDogru   -  felsefeYanlis   *   0.25
+    felsefeDogru = Number(document.getElementById("felsefeDogru").value);
+    felsefeYanlis = Number(document.getElementById("felsefeYanlis").value);
+    aytFelsefeNet = felsefeDogru   -  felsefeYanlis   *   0.25
     document.getElementById("aytFelsefeNet").innerHTML =aytFelsefeNet;
-    var felsefeToplam=felsefeDogru+felsefeYanlis
+    felsefeToplam=felsefeDogru+felsefeYanlis
     if(felsefeToplam>12){
         window.alert("Toplamda 12 soru girebilirsin.");
     }
 
-    var dinDogru = Number(document.getElementById("dinDogru").value);
-    var dinYanlis = Number(document.getElementById("dinYanlis").value);
-    var aytDinNet = dinDogru   -  dinYanlis   *   0.25
+    dinDogru = Number(document.getElementById("dinDogru").value);
+    dinYanlis = Number(document.getElementById("dinYanlis").value);
+    aytDinNet = dinDogru   -  dinYanlis   *   0.25
     document.getElementById("aytDinNet").innerHTML =aytDinNet;
-    var dinToplam=dinDogru+dinYanlis
+    dinToplam=dinDogru+dinYanlis
     if(dinToplam>6){
         window.alert("Toplamda 6 soru girebilirsin.");
     }
-
-    var matematikDogru = Number(document.getElementById("matematikDogru").value);
-    var matematikYanlis = Number(document.getElementById("matematikYanlis").value);
-    var aytMatematikNet = matematikDogru   -  matematikYanlis   *   0.25
+    
+    matematikDogru = Number(document.getElementById("matematikDogru").value);
+    matematikYanlis = Number(document.getElementById("matematikYanlis").value);
+    aytMatematikNet = matematikDogru   -  matematikYanlis   *   0.25
     document.getElementById("aytMatematikNet").innerHTML =aytMatematikNet;
-    var matematik2Toplam=matematikDogru+matematikYanlis
+    matematik2Toplam=matematikDogru+matematikYanlis
     if(matematik2Toplam>40){
         window.alert("Toplamda 40 soru girebilirsin.");
     }
 
-    var fizikDogru = Number(document.getElementById("fizikDogru").value);
-    var fizikYanlis = Number(document.getElementById("fizikYanlis").value);
-    var aytFizikNet = fizikDogru   -  fizikYanlis   *   0.25
+    fizikDogru = Number(document.getElementById("fizikDogru").value);
+    fizikYanlis = Number(document.getElementById("fizikYanlis").value);
+    aytFizikNet = fizikDogru   -  fizikYanlis   *   0.25
     document.getElementById("aytFizikNet").innerHTML =aytFizikNet;
-    var fizikToplam=fizikDogru+fizikYanlis
+    fizikToplam=fizikDogru+fizikYanlis
     if(fizikToplam>14){
         window.alert("Toplamda 14 soru girebilirsin.");
     }
 
-    var kimyaDogru = Number(document.getElementById("kimyaDogru").value);
-    var kimyaYanlis = Number(document.getElementById("kimyaYanlis").value);
-    var aytKimyaNet = kimyaDogru   -  kimyaYanlis   *   0.25
+    kimyaDogru = Number(document.getElementById("kimyaDogru").value);
+    kimyaYanlis = Number(document.getElementById("kimyaYanlis").value);
+    aytKimyaNet = kimyaDogru   -  kimyaYanlis   *   0.25
     document.getElementById("aytKimyaNet").innerHTML =aytKimyaNet;
-    var kimyaToplam=kimyaDogru+kimyaYanlis
+    kimyaToplam=kimyaDogru+kimyaYanlis
     if(kimyaToplam>13){
         window.alert("Toplamda 13 soru girebilirsin.");
     }
 
-    var BiyolojiDogru = Number(document.getElementById("BiyolojiDogru").value);
-    var BiyolojiYanlis = Number(document.getElementById("BiyolojiYanlis").value);
-    var aytBiyolojiNet = BiyolojiDogru   -  BiyolojiYanlis   *   0.25
+    BiyolojiDogru = Number(document.getElementById("BiyolojiDogru").value);
+    BiyolojiYanlis = Number(document.getElementById("BiyolojiYanlis").value);
+    aytBiyolojiNet = BiyolojiDogru   -  BiyolojiYanlis   *   0.25
     document.getElementById("aytBiyolojiNet").innerHTML =aytBiyolojiNet;
-    var BiyolojiToplam=BiyolojiDogru+BiyolojiYanlis
+    BiyolojiToplam=BiyolojiDogru+BiyolojiYanlis
     if(BiyolojiToplam>13){
         window.alert("Toplamda 13 soru girebilirsin.");
     }
@@ -169,40 +191,37 @@ elem.onclick = function() {
 /**
 * @brief  obp alert added
 */
-    var OBP = Number(document.getElementById("OBP").value);
-    var OBP;
-    if(OBP<0 ||OBP>100){
-        window.alert("OBP Puanı 0-100 aralığında olmalıdır");
+    Diplomanotu = Number(document.getElementById("Diplomanotu").value);
+    if(Diplomanotu < 0 || Diplomanotu > 100){
+        window.alert("Diploma Notu 0-100 aralığında olmalıdır");
     }
     
 /**
 * @brief  diploma grade calculated
 */
-    var OBP = Number(document.getElementById("OBP").value);
-    OBP=5*Diplomanotu
-    document.getElementById("Diplomanotu").innerHTML =Diplomanotu;
- 
-
-
-    
+    OBP = 5 * Diplomanotu
+    document.getElementById("OBP").value = OBP;
 }
 /**
 * @brief  cleared all textboxes with clear button
 */
-//TODO
+var temizle=document.getElementById("btnTemizle");
+temizle.onclick=function (){
+    document.getElementByType("number").innerHTML = "";
+}
 
 /**
 * @brief  the results are calculated with the calculate button
 */
-var submit=document.getElementById("btnHesapla")
-submit.onclick=function (){
+var hesapla=document.getElementById("btnHesapla");
+hesapla.onclick=function (){
 /**
 * @brief  raw score and placement score calculated
 */ 
     var tytHam= Turkcenet + sosyalnet + tmatematiknet + fennet;
     tytHam=tytHam*5
 
-    if(document.getElementById("checkbox").checked = true){  //If the checkbox is checked, the score calculation is done as in this section.
+    if(document.getElementById("checkbox").checked == true){  //If the checkbox is checked, the score calculation is done as in this section.
         var tytYerlestirme=Diplomanotu*0.3+tytHam;
     }
     else{//Default score calculation
@@ -213,7 +232,7 @@ submit.onclick=function (){
     sayHam=sayHam*6.25;
     sayHam=0.6*sayHam+0.4*tytHam;
 
-    if(document.getElementById("checkbox").checked = true){  //If the checkbox is checked, the score calculation is done as in this section.
+    if(document.getElementById("checkbox").checked == true){  //If the checkbox is checked, the score calculation is done as in this section.
         var sayYerlestirme=Diplomanotu*0.3+sayHam;
     }
     else{//Default score calculation
@@ -224,7 +243,7 @@ submit.onclick=function (){
     sözHam=sözHam*6.25;
     sözHam=0.6*sözHam+0.4*tytHam;
 
-    if(document.getElementById("checkbox").checked = true){  //If the checkbox is checked, the score calculation is done as in this section.
+    if(document.getElementById("checkbox").checked == true){  //If the checkbox is checked, the score calculation is done as in this section.
         var sözYerlestirme=Diplomanotu*0.3+sözHam;
     }
     else{//Default score calculation
@@ -233,7 +252,7 @@ submit.onclick=function (){
 
     var eaHam=(sözHam+12.5*aytMatematikNet)*0.5
     
-    if(document.getElementById("checkbox").checked = true){  //If the checkbox is checked, the score calculation is done as in this section.
+    if(document.getElementById("checkbox").checked == true){  //If the checkbox is checked, the score calculation is done as in this section.
         var eaYerlestirme=Diplomanotu*0.3+eaHam;
     }
     else{//Default score calculation
